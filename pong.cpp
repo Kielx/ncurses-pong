0c98c0f8ea249668171e26a795dcb2e943a0b548
Kielx <kiel66@gmail.com>
2022-01-24 21:57:28 +0100

    Basic game working

--- pong.cpp
+++ pong.cpp
@@ -4,27 +4,51 @@
 class Pong
 {
 public:
-  int xPos = 5;
-  int yPos = 5;
+  int xPos;
+  int yPos;
+  int width = 8;
+};
+
+class Ball
+{
+public:
+  int xPos;
+  int yPos;
+  int xVel;
+  int yVel;
 };
 
 int main(int argc, char *argv[])
 {
-  Pong mainPong;
-
-  WINDOW *okno;
-  okno = initscr();
-  nodelay(okno, 1);
+  int counter = 0;
+  WINDOW *okno;         /* Game window */
+  okno = initscr();     /* Start curses mode */
+  nodelay(okno, 1);     /* Dont wait for input */
   raw();                /* Line buffering disabled	*/
   keypad(stdscr, TRUE); /* We get F1, F2 etc..		*/
   noecho();             /* Don't echo() while we do getch */
-  curs_set(0);
+  curs_set(0);          /* Invisible cursor */
+
+  Pong mainPong; /* The main pong */
+  /* Set default position of main pong */
+  mainPong.yPos = LINES - 1;
+  mainPong.xPos = COLS / 2;
   mvprintw(mainPong.yPos, mainPong.xPos, "o");
+
+  Ball ball;
+  ball.xPos = COLS / 2;
+  ball.yPos = 0;
+  ball.xVel = 1;
+  ball.yVel = 1;
+  mvprintw(ball.yPos, ball.xPos, "o");
   refresh();
+
+  /* Steering loop */
   while (true)
   {
+    counter++;
     int key = getch();
-    if (key == 'k')
+    if (key == 'k' || key == 'q')
     {
       break;
     }
@@ -40,7 +64,7 @@ int main(int argc, char *argv[])
       refresh();
       clear();
     }
-    else if (key == KEY_UP)
+    /* else if (key == KEY_UP)
     {
       mainPong.yPos--;
       refresh();
@@ -51,8 +75,46 @@ int main(int argc, char *argv[])
       mainPong.yPos++;
       refresh();
       clear();
+    } */
+    clear();
+    if (counter % 20 == 0)
+    {
+      ball.xPos += ball.xVel;
+      ball.yPos += ball.yVel;
     }
-    mvprintw(mainPong.yPos, mainPong.xPos, "o");
+    if (ball.yPos == LINES - 2)
+    {
+      for (int i = mainPong.xPos; i < mainPong.xPos + mainPong.width; i++)
+      {
+        if (ball.xPos == i)
+        {
+          ball.yVel = -1;
+        }
+      }
+    }
+    if (ball.yPos == 0)
+    {
+      ball.yVel = 1;
+    }
+    if (ball.xPos == 0)
+    {
+      ball.xVel = 1;
+    }
+    if (ball.xPos == COLS - 1)
+    {
+      ball.xVel = -1;
+    }
+
+    mvprintw(ball.yPos, ball.xPos, "o");
+    for (int i = 0; i < mainPong.width; i++)
+    {
+      mvprintw(mainPong.yPos, mainPong.xPos + i, "o");
+    }
+
+    usleep(5 * 1000);
+
+    refresh();
   }
+
   endwin();
 }
