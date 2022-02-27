3295a67834a102fb1733743dcf65db04b0e8ebbd
Kielx <kiel66@gmail.com>
2022-02-27 17:58:32 +0100

    Update commits and code for 4 part of en-US tutorial

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 class Ball
 {
 public:
-  int x;
-  int y;
+  int x, y;
+  int x_speed, y_speed;
 };
 
 class Paddle
@@ -90,26 +90,108 @@ int move_paddle(WINDOW *win, Paddle &paddle)
   return 0;
 }
 
-void single_player(WINDOW *win)
+void ball_bounce(WINDOW *win, Ball &ball, Paddle &paddle, int &score)
 {
+  if (ball.y == getmaxy(win) - 3)
+  {
+    for (int i = paddle.x; i < paddle.x + paddle.width; i++)
+    {
+      if (ball.x == i)
+      {
+        ball.y_speed = -1;
+        score++;
+      }
+    }
+  }
+  if (ball.y == getbegx(win))
+  {
+    ball.y_speed = 1;
+  }
+  if (ball.x == getmaxx(win) - 2)
+  {
+    ball.x_speed = -1;
+  }
+  if (ball.x == getbegx(win))
+  {
+    ball.x_speed = 1;
+  }
+}
+
+/**
+ * @brief A function responsible for a single player game
+ *
+ * @param win - the window where the game takes place
+ *
+ * @return int - returns 1 if the user has lost
+ */
+int single_player(WINDOW *win)
+{
+  // Declare the ball and its initial position and speed
   Ball ball1;
   ball1.x = 10;
   ball1.y = 10;
-  mvwprintw(win, ball1.x, ball1.y, "o");
+  ball1.x_speed = 1;
+  ball1.y_speed = 1;
+  mvwprintw(win, ball1.y, ball1.x, "o");
+  // Declare the paddle, its position in the middle of the screen on the X axis and on the penultimate line of the Y axis
   Paddle paddle1;
   paddle1.x = getmaxx(win) / 2;
   paddle1.y = getmaxy(win) - 2;
   paddle1.width = 5;
+
+  // Counter allows you to move the ball with a delay
+  int counter = 0;
+  int score = 0;
+  // Loop that contains all the logic of the single player game
   while (true)
   {
+    counter++;
+    mvwprintw(win, 0, 0, "Score: %d", score / 300);
+    // Every 300 frames we move the ball, removing the old trail
+    if (counter % 300 == 0)
+    {
+      mvwprintw(win, ball1.y, ball1.x, " ");
+      ball1.x += ball1.x_speed;
+      ball1.y += ball1.y_speed;
+      mvwprintw(win, ball1.y, ball1.x, "o");
+    }
+
+    // Ball bounce function is called every frame
+    ball_bounce(win, ball1, paddle1, score);
+    if (ball1.y == getmaxy(win) - 1)
+    {
+      wclear(win);
+      box(win, 0, 0);
+      mvwprintw(win, getmaxy(win) / 2, getmaxx(win) / 2, "GAME OVER");
+      mvwprintw(win, getmaxy(win) / 2 + 1, getmaxx(win) / 2, "Your score: %d", score / 300);
+      mvwprintw(win, getmaxy(win) / 2 + 2, getmaxx(win) / 2, "Press any key to continue");
+      mvwprintw(win, getmaxy(win) / 2 + 3, getmaxx(win) / 2, "Press q to quit");
+      wrefresh(win);
+      nodelay(stdscr, FALSE);
+      int ch;
+      ch = getch();
+
+      if (ch == 'q' || ch == 'Q')
+        return 1;
+      else
+      {
+        wclear(win);
+        box(win, 0, 0);
+        nodelay(stdscr, TRUE);
+        return single_player(win);
+      }
+    }
+    // Function responsible for moving the paddle is called every frame
     int quit = move_paddle(win, paddle1);
     if (quit == 1)
     {
       break;
     }
+    // We put the program to sleep for 500 microseconds and refresh the window with new data
     usleep(500);
     wrefresh(win);
   }
+  return 0;
 }
 
 int main()
